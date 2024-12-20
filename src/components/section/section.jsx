7fabe6dfb8c0abef3c513aa511a170e5f616b7ef
Mert-Ozbolat <mertozbolat008@gmail.com>
2024-12-20 22:37:55 +0300

    updated

--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -1,16 +1,28 @@
 import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
 import homesStyle from '../../styles/home'
+import { titles } from '../../utils/constants'
 import Titles from '../titles/titles'
 import TrendMovies from '../../Categories/trendMovies'
-import { titles } from '../../utils/constants'
 import TopRatedMovies from '../../Categories/topRatedMovies'
 
 
 
-const Section = () => {
+const Section = (item) => {
+
+    const setData = () => {
+        switch (item.id) {
+            case 1:
+                return TrendMovies;
+            case 2:
+                return TopRatedMovies;
+            default:
+                return TopRatedMovies
+        }
+    }
 
     return (
         <View style={homesStyle.movieSection}>
+
             {
                 titles.map((title) => (
                     <View style={homesStyle.movies}>
@@ -20,6 +32,8 @@ const Section = () => {
                     </View>
                 ))
             }
+
+
         </View>
     )
 }
